48c601a081db2dabd242e4b5f624b08e133eabb1
Maryin.V <vasmar43rus@gmail.com>
2025-02-24 23:58:58 +0300

    добавление API методов в контроллер

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,18 +2,124 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Enum\Status;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/api/tasks', name: 'api_tasks_')]
 final class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): JsonResponse
+    private EntityManagerInterface $entityManager;
+    private TaskRepository $taskRepository;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TaskRepository $taskRepository
+    ) {
+        $this->entityManager = $entityManager;
+        $this->taskRepository = $taskRepository;
+    }
+
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function getAll(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TaskController.php',
-        ]);
+        $tasks = $this->taskRepository->findAll();
+        $data = [];
+
+        foreach ($tasks as $task) {
+            $data[] = $this->parseDataFromTask($task);
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function getById(int $id): JsonResponse
+    {
+        $task = $this->taskRepository->find($id);
+
+        if (!$task) {
+            return new JsonResponse(['error' => 'Task not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = $this->parseDataFromTask($task);
+
+        return $this->json($data);
+    }
+
+    #[Route('/', name: 'create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $task = new Task();
+        $task->setTitle($data['title']);
+        $task->setDescription($data['description']);
+        $task->setStatus(Status::tryFrom($data['status']));
+        $task->setCreatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($task);
+        $this->entityManager->flush();
+
+        $data = $this->parseDataFromTask($task);
+
+        return $this->json($data, Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['PUT'])]
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $task = $this->taskRepository->find($id);
+
+        if (!$task) {
+            return new JsonResponse(['error' => 'Task not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $task->setTitle($data['title'] ?? $task->getTitle());
+        $task->setDescription($data['description'] ?? $task->getDescription());
+        $task->setStatus(Status::tryFrom($data['status']) ?? $task->getStatus());
+        $task->setUpdatedAt(new \DateTime());
+
+        $this->entityManager->flush();
+
+        $data = $this->parseDataFromTask($task);
+
+        return $this->json($data, Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id): JsonResponse
+    {
+        $task = $this->taskRepository->find($id);
+
+        if (!$task) {
+            return new JsonResponse(['error' => 'Task not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($task);
+        $this->entityManager->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    private function parseDataFromTask(Task $task): array
+    {
+        return [
+            'id' => $task->getId(),
+            'title' => $task->getTitle(),
+            'description' => $task->getDescription(),
+            'status' => $task->getStatus()->value,
+            'created_at' => $task->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $task->getUpdatedAt()?->format('Y-m-d H:i:s'),
+        ];
     }
 }

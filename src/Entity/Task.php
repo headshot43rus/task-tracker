0ae46766a80357e7e8e6ddf6f87b0faa6a30aad3
Maryin.V <vasmar43rus@gmail.com>
2025-02-24 23:55:18 +0300

    правка Entity/Task

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -6,33 +6,49 @@ use App\Enum\Status;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Table(name: 'tasks')]
+#[ORM\HasLifecycleCallbacks]
 class Task
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['tasks'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tasks'])]
     private ?string $title = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(length: 255)]
+    #[Groups(['tasks'])]
     private ?string $description = null;
 
     #[ORM\Column(enumType: Status::class)]
+    #[Groups(['tasks'])]
     private ?Status $status = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $created_at = null;
-
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['tasks'])]
+    private ?\DateTimeInterface $created_at = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[Groups(['tasks'])]
     private ?\DateTimeInterface $updated_at = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
     }
 
     public function getTitle(): ?string
@@ -71,12 +87,12 @@ class Task
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->created_at;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    public function setCreatedAt(\DateTimeInterface $created_at): static
     {
         $this->created_at = $created_at;
 
@@ -88,10 +104,16 @@ class Task
         return $this->updated_at;
     }
 
-    public function setUpdatedAt(\DateTimeInterface $updated_at): static
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): static
     {
         $this->updated_at = $updated_at;
 
         return $this;
+    }
+
+    #[ORM\PreUpdate]
+    public function onPreUpdate(): void
+    {
+        $this->updated_at = new \DateTime();
     }
 }
--- src/Enum/Status.php
+++ src/Enum/Status.php
@@ -5,6 +5,6 @@ namespace App\Enum;
 enum Status: string
 {
     case NEW = 'new';
-    case IN_PROGRESS = 'in progress';
+    case IN_PROGRESS = 'in_progress';
     case DONE = 'done';
 }
